9babf3737cb5f50afecc05c4f9f0ad22688acf58
JeukHwang <92910273+JeukHwang@users.noreply.github.com>
2023-02-01 01:43:28 +0900

    [Gold IV] Title: 여행 가자, Time: 68 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Gold/1976. 여행 가자/여행 가자.py
@@ -0,0 +1,22 @@
+N, M = int(input()), int(input())
+S = [list(map(int, input().split())) for _ in range(N)]
+E = [int(v) - 1 for v in input().split()]
+
+L = [i for i in range(N)]
+
+
+def get(n):
+    if L[n] != n:
+        L[n] = get(L[n])
+    return L[n]
+
+
+def union(n, m):
+    L[get(m)] = get(n)
+
+
+for i in range(N - 1):
+    for j in range(i, N):
+        if S[i][j] == 1:
+            union(i, j)
+print("YES" if all(get(e) == get(E[0]) for e in E) else "NO")
