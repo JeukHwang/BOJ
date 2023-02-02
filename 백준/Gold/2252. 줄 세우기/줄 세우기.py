b33c498ada5bfb21db13a3c43d59c5279c1ea2f9
JeukHwang <92910273+JeukHwang@users.noreply.github.com>
2023-02-02 15:05:49 +0900

    [Gold III] Title: 줄 세우기, Time: 184 ms, Memory: 38444 KB -BaekjoonHub

--- 백준/Gold/2252. 줄 세우기/줄 세우기.py
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.py
@@ -8,11 +8,13 @@ for _ in range(M):
     larger[A].append(B)
     smaller[B] += 1
 
-queue = [i for i in range(1, N + 1) if smaller[i] == 0]
-while queue:
-    i = queue.pop()
+stack = [i for i in range(1, N + 1) if smaller[i] == 0]
+L = []
+while stack:
+    i = stack.pop()
     for v in larger[i]:
         smaller[v] -= 1
         if smaller[v] == 0:
-            queue.append(v)
-    print(i, end=" ")
+            stack.append(v)
+    L.append(i)
+print(*L)
