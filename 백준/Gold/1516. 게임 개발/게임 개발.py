229a9bf8aa6b058ef12887d7e7f6e685b356591a
JeukHwang <92910273+JeukHwang@users.noreply.github.com>
2023-02-03 02:19:36 +0900

    [Gold III] Title: 게임 개발, Time: 52 ms, Memory: 32276 KB -BaekjoonHub

--- 백준/Gold/1516. 게임 개발/게임 개발.py
+++ 백준/Gold/1516. 게임 개발/게임 개발.py
@@ -2,18 +2,16 @@ import sys
 
 I = sys.stdin.readline
 N = int(I())
-G = [None] * (N + 1)
+T, G = [-1] * (N + 1), [0] * (N + 1)
 for i in range(1, N + 1):
-    t, *ns, _ = map(int, I().split())
-    G[i] = (t, ns)
-T = [-1] * (N + 1)
+    T[i], *G[i], _ = map(int, I().split())
+t = [-1] * (N + 1)
 
 
 def get(i):
-    if T[i] == -1:
-        t, ns = G[i]
-        T[i] = (max(get(n) for n in ns) if ns else 0) + t
-    return T[i]
+    if t[i] == -1:
+        t[i] = (max(get(n) for n in G[i]) if G[i] else 0) + T[i]
+    return t[i]
 
 
 print("\n".join(str(get(i)) for i in range(1, N + 1)))
