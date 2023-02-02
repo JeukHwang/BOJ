8a7713cd8e514a124049c26c65c35d2a7bd7de03
JeukHwang <92910273+JeukHwang@users.noreply.github.com>
2023-02-03 02:37:46 +0900

    [Gold III] Title: 게임 개발, Time: 52 ms, Memory: 32276 KB -BaekjoonHub

--- 백준/Gold/1516. 게임 개발/게임 개발.py
+++ 백준/Gold/1516. 게임 개발/게임 개발.py
@@ -5,11 +5,11 @@ N = int(I())
 T, G = [-1] * (N + 1), [0] * (N + 1)
 for i in range(1, N + 1):
     T[i], *G[i], _ = map(int, I().split())
-t = [-1] * (N + 1)
+t = [0] * (N + 1)
 
 
 def get(i):
-    if t[i] == -1:
+    if t[i] == 0:
         t[i] = (max(get(n) for n in G[i]) if G[i] else 0) + T[i]
     return t[i]
 
