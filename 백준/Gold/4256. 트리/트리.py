c68189d15b68160cfeeb236dceddbe956acf15fc
JeukHwang <92910273+JeukHwang@users.noreply.github.com>
2023-02-05 16:14:31 +0900

    [Gold II] Title: 트리, Time: 316 ms, Memory: 31412 KB -BaekjoonHub

--- 백준/Gold/4256. 트리/트리.py
+++ 백준/Gold/4256. 트리/트리.py
@@ -1,25 +1,16 @@
-def postorder(pre_s, pre_f, in_s, in_f):
+def postorder(pre_s, in_s, size):
     global preorder, inverse_inorder, answer, counter
-    if pre_f - pre_s == 0:
+    if size == 0:
         return
-    if pre_f - pre_s == 1:
+    if size == 1:
         answer[counter] = preorder[pre_s]
         counter += 1
     else:
         i = inverse_inorder[preorder[pre_s]]
-        l, r = i - in_s, in_f - i - 1
-        postorder(
-            pre_s + 1,
-            pre_s + 1 + l,
-            in_s,
-            i,
-        )
-        postorder(
-            pre_f - r,
-            pre_f,
-            i + 1,
-            in_f,
-        )
+        l = i - in_s
+        r = size - l - 1
+        postorder(pre_s + 1, in_s, i - in_s)
+        postorder(pre_s + 1 + l, i + 1, r)
         answer[counter] = preorder[pre_s]
         counter += 1
 
@@ -32,5 +23,5 @@ for _ in range(int(input())):
     for i in range(N):
         inverse_inorder[inorder[i]] = i
     answer, counter = [0] * N, 0
-    postorder(0, N, 0, N)
+    postorder(0, 0, N)
     print(*answer)
