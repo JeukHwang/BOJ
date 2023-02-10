3ccda405226b7596da757be48c5adc858a12ad0c
JeukHwang <92910273+JeukHwang@users.noreply.github.com>
2023-02-10 23:14:32 +0900

    [Gold IV] Title: 전화번호 목록, Time: 152 ms, Memory: 32276 KB -BaekjoonHub

--- 백준/Gold/5052. 전화번호 목록/전화번호 목록.py
+++ 백준/Gold/5052. 전화번호 목록/전화번호 목록.py
@@ -3,6 +3,6 @@ import sys
 I = sys.stdin.readline
 for _ in range(int(I())):
     n = int(I())
-    L = sorted(I().rstrip() for _ in range(n))
+    L = sorted(I().strip() for _ in range(n))
     is_consistant = any(L[i] == L[i + 1][: len(L[i])] for i in range(n - 1))
     print("YES" if not is_consistant else "NO")
