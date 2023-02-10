6063d8912ec170bbb6c26df8aec5240bc98b1b8d
JeukHwang <92910273+JeukHwang@users.noreply.github.com>
2023-02-10 23:09:46 +0900

    [Gold IV] Title: 전화번호 목록, Time: 168 ms, Memory: 32276 KB -BaekjoonHub

--- 백준/Gold/5052. 전화번호 목록/전화번호 목록.py
+++ 백준/Gold/5052. 전화번호 목록/전화번호 목록.py
@@ -5,8 +5,7 @@ for _ in range(int(I())):
     n = int(I())
     L = sorted(I().rstrip() for _ in range(n))
     for i in range(n - 1):
-        l1, l2 = len(L[i]), len(L[i + 1])
-        if l1 < l2 and L[i][l1 - 1] == L[i + 1][l1 - 1] and L[i] == L[i + 1][:l1]:
+        if L[i] == L[i + 1][: len(L[i])]:
             print("NO")
             break
     else:
